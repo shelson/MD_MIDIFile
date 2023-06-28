7a3ae29c3b70d2f6661bb490752060bca582699c
Simon Helson <simon.helson@gmail.com>
2023-06-28 12:53:22 +0100

    Maybe?

--- src/MD_MIDITrack.cpp
+++ src/MD_MIDITrack.cpp
@@ -284,8 +284,8 @@ void MD_MFTrack::parseEvent(MD_MIDIFile *mf)
 
       case 0x58:  // time signature
       {
-        uint8_t n = mf->_fd.readBytes();
-        uint8_t d = mf->_fd.readBytes();
+        uint8_t n = mf->_fd.readBytes(1);
+        uint8_t d = mf->_fd.readBytes(1);
         
         mf->setTimeSignature(n, 1 << d);  // denominator is 2^n
         mf->_fd.seek(mLen - 2, SeekCur);
