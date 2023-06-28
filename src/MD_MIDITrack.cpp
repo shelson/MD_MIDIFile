2b0a54f6c5ec90d033d7758d0178653c28220d3b
Simon Helson <simon.helson@gmail.com>
2023-06-28 16:52:46 +0100

    sdf

--- src/MD_MIDITrack.cpp
+++ src/MD_MIDITrack.cpp
@@ -421,7 +421,7 @@ void MD_MFTrack::parseEvent(MD_MIDIFile *mf)
         uint8_t minLen = min(ARRAY_SIZE(mev.data), mLen);
         
         for (uint8_t i = 0; i < minLen; ++i)
-          mf->_fd.readBytes((char *) mev.data[i], sizeof(char)); // read next
+          mf->_fd.readBytes((char *) &mev.data[i], sizeof(char)); // read next
 
         mev.chars[minLen] = '\0'; // in case it is a string
         if (mLen > ARRAY_SIZE(mev.data))
